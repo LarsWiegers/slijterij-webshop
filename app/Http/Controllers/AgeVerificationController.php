28e8888fc7adf50d83677018c2191b5196a8e0a8
Lars Wiegers <larswiegers@live.nl>
2017-09-10 14:50:41 +0200

    Fix a small mistake when setting the age_verification session

--- app/Http/Controllers/AgeVerificationController.php
+++ app/Http/Controllers/AgeVerificationController.php
@@ -19,11 +19,11 @@ class AgeVerificationController extends Controller
 	public function validation($boolean) {
 		switch($boolean) {
 			case 'yes':
-				session(["age_verification", true]);
+				session(["age_verification" => true]);
 				return redirect()->route("home");
 				break;
 			case 'no':
-				session(["age_verification", false]);
+				session(["age_verification" => false]);
 				return redirect()->route("age_restriction_result_false");
 				break;
 			default:
