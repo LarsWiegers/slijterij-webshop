85c1303f6552cc8ef7f8df7b1d5a37d68fbbfc16
Lars Wiegers <larswiegers@live.nl>
2017-09-14 11:24:14 +0200

    Sign up bar

--- resources/views/components/signUpNewsLetterBar.blade.php
+++ resources/views/components/signUpNewsLetterBar.blade.php
@@ -1,7 +1,33 @@
-<section class="sign_up_bar">
-    {!! Form::open(["url" => route("signUp_url")]) !!}
-        <label for="naam">Uw naam: </label><input value="{{Auth::user()->name}}" id="naam">
-        <label for="achternaam">Uw achternaam: </label><input value="{{Auth::user()->achternaam}}" id="achternaam">
-        <label for="email">Uw email adress: </label><input value="{{Auth::user()->email}}" id="email">
+<section class="sign_up_bar container-fluid">
+    {!! Form::open(["url" => route("sign_up_url")]) !!}
+    <div class="container has-text-centered">
+        <h1>Wilt u meer informatie?</h1>
+        <h3>Schrijf u dan in voor onze email lijst</h3>
+    </div>
+    <div class="columns">
+        <div class="column">
+            <span class="for-layout">
+                <label for="naam">Uw naam: </label>
+            </span>
+            <input value="{{isset(Auth::user()->first_name) ? Auth::user()->first_name : '' }}" id="naam">
+        </div>
+        <div class="column">
+            <span class="for-layout">
+                <label for="achternaam">Uw achternaam: </label>
+            </span>
+            <input value="{{isset(Auth::user()->last_name) ? Auth::user()->last_name : '' }}" id="achternaam">
+        </div>
+        <div class="column">
+            <span class="for-layout">
+                <label for="email">Uw email adress: </label>
+            </span><input value="{{isset(Auth::user()->email) ? Auth::user()->email : '' }}" id="email">
+        </div>
+        <div class="column is-one-third">
+            <span class="for-layout">
+                stuur mij e-mails
+            </span>
+            <button type="submit" value="Submit" class="button">Subscribe</button>
+        </div>
+    </div>
     {!! Form::close() !!}
 </section>
--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::POST("/sign_up","sigUpController@index")->name("signUp_ur");
+Route::POST("/sign_up","sigUpController@index")->name("sign_up_url");
