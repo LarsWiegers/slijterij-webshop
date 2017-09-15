f88afb0a72e5d77f1c5be46ce6d9e0ddef65542a
Lars Wiegers <larswiegers@live.nl>
2017-09-15 11:25:28 +0200

    More comments

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,25 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    //
-	public function index() {
-		$product = Product::find(1);
-		$product->setAttribute("productImages",$product->getProductImages() );
-		return view("product.index",["product" => $product]);
-	}
-	public function searchProduct($productName) {
-		$product = Product::where("name", 'LIKE' , "%" . $productName . "%")->get()[0];
-		if(! count( $product ) ) {
-			// if product does not exist
-			return view("product.does_not_exist");
-		}
+	/**
+	 *
+	 * looks for the wanted product
+	 * and returns a view that displays it
+	 *
+	 * @param       String
+	 * @return      View
+	 *
+	 */
+    public function searchProduct($productName) {
+		$product = (new Product())->where("name", 'LIKE' , "%" . $productName . "%")
+		                          ->firstOrFail();
 		$product->productImages = $product->getProductImages();
-
 		return view("product.index",["product" => $product]);
-
-
 	}
 }
